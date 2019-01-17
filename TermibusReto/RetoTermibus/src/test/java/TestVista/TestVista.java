b6679415495992126ec158285c56c54033f4265c
Ledesin <46573074+Ledesin@users.noreply.github.com>
2019-01-17 20:24:17 +0100

    Creacion repo

---
+++ TermibusReto/RetoTermibus/src/test/java/TestVista/TestVista.java
@@ -0,0 +1,14 @@
+package TestVista;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestVista {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
