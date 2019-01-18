eb3527eee177c61fcc2eebdd152a006138763088
unknown <sergio.ledesmadi@elorrieta-errekamari.com>
2019-01-18 15:47:23 +0100

    Bienvenida final

--- TermibusReto/RetoTermibus/src/main/java/Vista/Bienvenida.java
+++ TermibusReto/RetoTermibus/src/main/java/Vista/Bienvenida.java
@@ -1,43 +1,33 @@
 package Vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Bienvenida extends JFrame {
-
-	private JPanel PanelBienvenida;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Vista frame = new Vista();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class bienvenida extends JPanel {
 
 	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
-	public Bienvenida() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		PanelBienvenida = new JPanel();
-		PanelBienvenida.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(PanelBienvenida);
-		PanelBienvenida.setLayout(null);
+	public bienvenida() {
+		setLayout(null);
+		
+		JLabel lblBienvenida1 = new JLabel("Logo");
+		lblBienvenida1.setIcon(new ImageIcon(bienvenida.class.getResource("/imagenes/logo-termibus.png")));
+		lblBienvenida1.setBounds(75, 48, 303, 117);
+		add(lblBienvenida1);
+		
+		JLabel lblBienvenida2 = new JLabel("Pulsa para acceder al Menu");
+		lblBienvenida2.setBounds(143, 176, 131, 21);
+		add(lblBienvenida2);
+		
+		JButton btnBienvenida = new JButton("");
+		btnBienvenida.setSelectedIcon(null);
+		btnBienvenida.setIcon(new ImageIcon(bienvenida.class.getResource("/imagenes/luzazul.jpeg")));
+		btnBienvenida.setBounds(0, 0, 450, 300);
+		add(btnBienvenida);
+		
+		
 	}
 }
